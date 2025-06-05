30e67bd825fdfe603b9597d15509d1c6990dc008
Diego Sava <diego.sava@clearpeaks.com>
2025-06-05 12:41:27 +0200

    running

---
+++ src/main.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, send_from_directory
+import yaml
+import os
+from sharepoint import SharePoint
+import webbrowser
+from flask import request
+
+app = Flask(__name__, static_folder='../static', template_folder='../templates')
+
+def read_config() -> dict:
+    config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "config.yml")
+    with open(config_path) as stream:
+        try:
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            raise Exception("Error reading configuration file", exc)
+
+def download_excel():
+    config = read_config()
+    sp = SharePoint(
+        config["SharePoint"]["hostname"],
+        config["SharePoint"]["site_name"],
+        config["SharePoint"]["app_registration"]["client_id"],
+        os.environ[config["SharePoint"]["app_registration"]["client_secret"]],
+        config["SharePoint"]["app_registration"]["tenant_id"]
+    )
+
+    file_url = config["SharePoint"]["innomap_doc"]["sp_link"]
+    save_path = os.path.join(os.path.dirname(__file__), "..", "static", "SynvertInnoMapTracker.xlsx")
+
+    if os.path.exists(save_path):
+        os.remove(save_path)
+        print(f"Deleted existing file: {save_path}")
+
+    sp.download_xlsx(file_url, save_path)
+    print(f"Downloaded file to: {save_path}")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/SynvertInnoMapTracker.xlsx")
+def serve_excel():
+    return send_from_directory("../static", "SynvertInnoMapTracker.xlsx")
+
+@app.route("/refresh-data")
+def refresh_data():
+    token = request.args.get("token")
+    if token != os.environ.get("REFRESH_TOKEN"):
+        return "Unauthorized", 403
+
+    try:
+        download_excel()
+        return "Excel updated successfully!"
+    except Exception as e:
+        return f"Error: {str(e)}", 500
+
+if __name__ == "__main__":
+    download_excel()
+    webbrowser.open("http://localhost:8000")
+    app.run(host="0.0.0.0", port=8000)
--- tests/unit/test_example.py
+++
@@ -1,5 +0,0 @@
-from src.example import my_sum
-
-
-def test_sum() -> None:
-    assert my_sum(3, 2) == 5
